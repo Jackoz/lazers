ba86822c6797f71e3d9315af0a920747466454ac
Jack <jack@pixbits.com>
2018-12-30 02:50:32 +0100

    added tests for laser rendering

--- src/platforms/gba/main.cpp
+++ src/platforms/gba/main.cpp
@@ -20,6 +20,7 @@ alignas(4) color_t shipPalette[] = { color_t(7, 9, 10), color_t(10, 12, 13), col
 alignas(4) color_t mirrorPalette[] = { color_t(0, 0, 0), color_t(5, 6, 6), color_t(8, 8, 8), color_t(10, 10, 11), color_t(13, 14, 14), color_t(13, 14, 15), color_t(15, 15, 15), color_t(16, 18, 19), color_t(20, 21, 22), color_t(20, 22, 22), color_t(20, 22, 23), color_t(22, 23, 24), color_t(24, 26, 27), color_t(25, 28, 29), color_t(28, 30, 31), color_t(30, 31, 31) };
 
 // prismPalette color_t palette[] = { color_t(0, 0, 0), color_t(7, 5, 8), color_t(10, 7, 12), color_t(9, 11, 12), color_t(12, 8, 14), color_t(18, 15, 20), color_t(19, 20, 21), color_t(20, 22, 22), color_t(21, 23, 24), color_t(23, 25, 26), color_t(24, 25, 26), color_t(25, 26, 27), color_t(26, 28, 29), color_t(26, 28, 29), color_t(27, 28, 29), color_t(31, 31, 32) };
+alignas(4) const color_t laserPalette[] = { color_t(0, 0, 0), color_t(26, 26, 26), color_t(30, 30, 30), color_t(29, 25, 0), color_t(31, 28, 0), color_t(0, 26, 0), color_t(7, 31, 7), color_t(8, 30, 31), color_t(0, 23, 26), color_t(1, 19, 29), color_t(8, 20, 31), color_t(25, 0, 23), color_t(31, 8, 26), color_t(26, 3, 7), color_t(31, 3, 6), color_t(0, 0, 0) };
 
 
 
@@ -30,6 +31,16 @@ Gfx gfx;
 
 class LevelView
 {
+public:
+  enum
+  {
+    MIRROR_SPRITE = 12,
+    PRISM_SPRITE = 28,
+    SOURCE_SPRITE = 28 + 16,
+    
+    LASER_RED = SOURCE_SPRITE + 16
+  };
+  
 public:
   void drawPiece(tile_entry* dest, u32 x, u32 y, Piece* piece)
   {
@@ -45,14 +56,21 @@ public:
       case PIECE_MIRROR:
       {
         rotated = o >= 4;
-        index = 12 + (o%4)*4;
+        index = MIRROR_SPRITE + (o%4)*4;
         break;
       }
         
       case PIECE_PRISM:
       {
         rotated = o >= 4;
-        index = 28 + (o%4)*4;
+        index = PRISM_SPRITE + (o%4)*4;
+        break;
+      }
+        
+      case PIECE_SOURCE:
+      {
+        rotated = o >= 4;
+        index = SOURCE_SPRITE + (o%4)*4;
         break;
       }
     }
@@ -106,6 +124,7 @@ public:
     {
       field.place(Pos(2+i, 2), new Mirror((Direction)i));
       field.place(Pos(2+i, 3), new Prism((Direction)i));
+      field.place(Pos(2+i, 6), new LaserSource((Direction)i, COLOR_RED));
     }
   }
 };
@@ -129,8 +148,7 @@ int main(void)
   port_disp_cnt* dispCnt = gfx.dispCnt();
   dispCnt->set(0);
   dispCnt->setMode(video_mode::MODE0);
-  dispCnt->enableBG0();
-  dispCnt->enableBG1();
+  dispCnt->enableBGs();
   dispCnt->enableOBJ();
   dispCnt->setObjMapping1D();
   
@@ -139,20 +157,38 @@ int main(void)
   bg0cnt->setScreenSize(bg_screen_size::SIZE32x32);
   bg0cnt->setTileDataBlock(0);
   bg0cnt->setTileMapBlock(16);
-  bg0cnt->setPriority(1);
+  bg0cnt->setPriority(3);
   
   port_bg_cnt* bg1cnt = gfx.bg1Cnt();
   bg1cnt->set(0);
   bg1cnt->setScreenSize(bg_screen_size::SIZE32x32);
   bg1cnt->setTileDataBlock(0);
   bg1cnt->setTileMapBlock(17);
-  bg1cnt->setPriority(0);
+  bg1cnt->setPriority(2);
+  
+  port_bg_cnt* bg2cnt = gfx.bg2Cnt();
+  bg2cnt->set(0);
+  bg2cnt->setScreenSize(bg_screen_size::SIZE32x32);
+  bg2cnt->setTileDataBlock(0);
+  bg2cnt->setTileMapBlock(18);
+  bg2cnt->setPriority(1);
+  
+  port_bg_cnt* bg3cnt = gfx.bg3Cnt();
+  bg3cnt->set(0);
+  bg3cnt->setScreenSize(bg_screen_size::SIZE32x32);
+  bg3cnt->setTileDataBlock(0);
+  bg3cnt->setTileMapBlock(19);
+  bg3cnt->setPriority(0);
   
   gfx.bg0().setOffset(0, 0);
   gfx.bg1().setOffset(0, 0);
+  gfx.bg2().setOffset(-4, 0);
+  gfx.bg3().setOffset(0, -4);
   
   memcpy(&gfx.getBgPalette(0), shipPalette, PALETTE_SMALL_SIZE * sizeof(color_t));
   memcpy(&gfx.getBgPalette(1), mirrorPalette, PALETTE_SMALL_SIZE * sizeof(color_t));
+  memcpy(&gfx.getBgPalette(2), laserPalette, PALETTE_SMALL_SIZE * sizeof(color_t));
+
   
   memset(&gfx.getObjPalette(0), 0, PALETTE_SMALL_SIZE * sizeof(color_t));
   gfx.getObjPalette(0)[1] = color_t(5, 31, 5);
@@ -177,6 +213,14 @@ int main(void)
       tileMap[y*32 + x].set(4 + (x & 0x01) | ((y & 0x01) << 1));
     }
   
+  auto* laserMap = gfx.getBgTileMap(18);
+  laserMap[0].set(LevelView::LASER_RED, 4, false, false);
+  laserMap[32].set(LevelView::LASER_RED, 4, false, false);
+  
+  auto* laserMap2 = gfx.getBgTileMap(19);
+  laserMap2[0].set(LevelView::LASER_RED+3 + 2, 4, false, false);
+  laserMap2[1].set(LevelView::LASER_RED+3 + 2, 4, false, false);
+
   levelView.refresh();
   
   oam_entry object;
